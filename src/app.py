f4471961de67f8772f041cb7a5f22ec28264dd70
JacobNakayu <jacob.nakayu@gmail.com>
2025-04-10 18:32:40 -0600

    Added a page that lists instructions for operation and made a second executable

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import shelve
 import dbm.dumb
 import sys
 # Fonts
-from styles import MAIN_TITLE
+from styles import MAIN_TITLE, BODY_TEXT
 # Custom Widgets
 from widgets import ScrollableFrame, FileTree, FilterListTree
 # Global variables
@@ -32,10 +32,11 @@ class DustFoxApp(tk.Tk):
         self.title("Dust Fox")
         
         # Navigational Menu
-        menu_bar = tk.Menu(self)
-        menu_bar.add_command(label="Scan", command=lambda: self.show_frame(ScanPage))
-        menu_bar.add_command(label="Settings", command=lambda: self.show_frame(SettingsPage))
-        self.config(menu=menu_bar)
+        self.menu_bar = tk.Menu(self)
+        self.menu_bar.add_command(label="Scan", background="GREEN",command=lambda: self.show_frame(ScanPage))
+        self.menu_bar.add_command(label="Settings", command=lambda: self.show_frame(SettingsPage))
+        self.menu_bar.add_command(label="Instructions", command=lambda:self.show_frame(InstructionsPage))
+        self.config(menu=self.menu_bar)
         
         # Create a Scrollable Frame to hold the content
         main_frame = ScrollableFrame(self, MIN_WIDTH, MIN_HEIGHT)
@@ -53,7 +54,7 @@ class DustFoxApp(tk.Tk):
         self.frames = {}
         
         # Initialize each page into the frames dictionary
-        for F in (ScanPage, SettingsPage):
+        for F in (ScanPage, SettingsPage, InstructionsPage):
             # Set the parent of the page to be the main container and the controller to be the app
             frame = F(content_frame, self)
             # Puts the view in the dictionary using the class as the key
@@ -73,7 +74,8 @@ class DustFoxApp(tk.Tk):
         try:
             frame.get_settings()
         except:
-           pass 
+           pass
+       
         
         # Bring that page to the top of the stack
         frame.tkraise()
@@ -94,20 +96,24 @@ class ScanPage(tk.Frame):
         for i in range(0,3):
             self.grid_columnconfigure(i, weight=1)
         
-        for i in range(0,4):
+        for i in range(0,5):
             self.grid_rowconfigure(i, weight=1)
+            
+        # Title Label
+        page_title = ttk.Label(self, text="Scan", font = MAIN_TITLE)
+        page_title.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
         
         # Button to run scan
         scan_button = ttk.Button(self, text="Scan for Old Files", command=lambda: self.scan_from_click())
-        scan_button.grid(row=0, column=0, columnspan=3, pady=10)
+        scan_button.grid(row=1, column=0, columnspan=3, pady=10)
         
         # Current directory label
         self.cwd_label = ttk.Label(self, text=("No Scan Results"), background="white", anchor="center")
-        self.cwd_label.grid(row=1, column=0, columnspan=3, pady=10, sticky="we")
+        self.cwd_label.grid(row=2, column=0, columnspan=3, pady=10, sticky="we")
         
         # Results widget
         results_panel = ScrollableFrame(self, 500, 0)
-        results_panel.grid(row=2, column=0, columnspan=3, padx=10, sticky="we")
+        results_panel.grid(row=3, column=0, columnspan=3, padx=10, sticky="we")
         self.results_frame = results_panel.scrollable_frame
         self.results_frame.config(bg="white")
     
@@ -115,11 +121,11 @@ class ScanPage(tk.Frame):
         
         # Action Buttons
         delete_button = ttk.Button(self, text="Delete Files", command=lambda: deleteFiles(self.results_tree))
-        delete_button.grid(row=3, column=0, padx=10, pady=10)
+        delete_button.grid(row=4, column=0, padx=10, pady=10)
         open_file_button = ttk.Button(self, text="Open Files", command=lambda: openFiles(self.results_tree))
-        open_file_button.grid(row=3, column=1, padx=10, pady=10)
+        open_file_button.grid(row=4, column=1, padx=10, pady=10)
         exclude_file_button = ttk.Button(self, text="Exclude Files", command=lambda: excludeFiles(self.results_tree))
-        exclude_file_button.grid(row=3, column=2, padx=10, pady=10)
+        exclude_file_button.grid(row=4, column=2, padx=10, pady=10)
     
     def scan_from_click(self):
         self.scan_results = runScan()
@@ -148,11 +154,7 @@ class SettingsPage(tk.Frame):
             
         for i in range (0,7):
             self.grid_rowconfigure(i, weight=1)
-            
-        # Configure rows to allow vertical centering 
-        # Makes the space above and below the content expand when the window enlarges
-        # self.grid_rowconfigure(0, weight=1)  # Space above title
-        # self.grid_rowconfigure(4, weight=1)  # Space below buttons
+
         
         # Title of the page (text, font, and placement)
         page_title = ttk.Label(self, text="Settings", font = MAIN_TITLE)
@@ -269,6 +271,84 @@ class SettingsPage(tk.Frame):
     def revert(self):
         revert_settings()
         self.get_settings()
+        
+
+class InstructionsPage(tk.Frame):
+    # Pass in the parent container and the controlling application
+    def __init__(self, parent, controller):
+        # Initialize the Frame class
+        super().__init__(parent)
+        
+        # Configure the grid
+        for i in range(0,3):
+            self.grid_columnconfigure(i, weight=1)
+        
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)           
+        
+            
+        # Title Label
+        page_title = ttk.Label(self, text="Instructions", font = MAIN_TITLE)
+        page_title.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+        
+        # Instructions Frame
+        # instructions_panel = ScrollableFrame(self, 450, 0)
+        # instructions_panel.grid(row=3, column=0, columnspan=3, rowspan=4, padx=10, sticky="nsew")
+        # self.instructions_frame = instructions_panel.scrollable_frame
+        
+        # self.instructions_frame.grid_columnconfigure(0, weight=1)
+        # self.instructions_frame.grid_rowconfigure(0, weight=1)   # Title might not need to expand
+
+        
+        # Instructions 
+        instructions = tk.Text(self, wrap="word")
+        scan_title = "Scanning and Results\n\n"
+        scan_instructions= """To run a scan, simply click the \"Scan for Old Files\" button at the top of the Scan page. Dust Fox will search through your allowed directories and collect information on files you may have forgotten.
+
+This process may take a little while, during which time the window may say that the proram is not responding. This is normal.
+
+Once the scan is complete, Dust Fox will display the name of your home directory in the white bar that previously said \"No Scan Results\", and the results of the scan in a collapseable tree in the large white box beneath that.
+
+Each directory will show its name and a + sign that you can click to expand it. Files will be displayed with their names, size, and the date and time they were created, last modified, and last opened. Use the horizontal scroll bar or enlarge the window if you cannot see everything.
+
+Any items highlighted blue in the scan results are considered \"selected\" by you. You can select multiple items by holding the CTRL button as you click files and directories.
+
+Once you have selected one or more files or directories, you can click one of the buttons below the results box. Clicking the \"Delete\" button will move the file or folder into your Trash or Recycling bin. Clicking \"Open Files\" will open the file in your operating system's default application for that file type. Clicking \"Exclude Files\" will remove the file or directory from the results and add it to the list of files and directories to skip over when scanning."""
+
+        settings_title = "\n\nConfiguring Settings\n\n"
+
+        settings_instructions = """The Settings page has several options that you can customize to ensure that your scan works the way you want.
+
+At the top, there is a dropdown menu labeled \"Look for Files Not Touched In\". This is the setting that defines what an \"old file\" is. Dust Fox will only report files that have been neither accessed nor modified within the specified date range. This is set to 30 days by default.
+
+Next is a dropdown labeled \"Scan Hidden Directories and Files\", which is set to False by default. Hidden directories and files are ones whose names begin with a \".\" and are usually configuration files or application data that shouldn't be touched except by the program. The settings for Dust Fox are stored in a hidden diretory called `./dust_fox` in your home directory. Changing this setting to True may clutter your scan results, but will offer much greater visibility into what files are lurking on your computer.
+
+After that, there are two nearly identical blocks labeled \"Scan These\" and \"Do Not  Scan\". Only the first box is populated by default. When Dust Fox is scanning your computer, if will first chech to make sure that the directory or file is within a larger directory listed in the \"Scan These\" box. Then, it will make sure that it is not listed in the \"Do Not Scan\" box. As with the scan results, you can select or multi-select items listed in the boxes and delete them from their list by clicking the \"Remove Selected\" button. You can also manually add file paths to these lists by typing them in to the text box next to the \"Add Path\" button and then clicking said button. You can type anything you want inthere, but Dust Fox will only filter using real file paths (since it\'s just checking string variables). Also, note that Dust Fox will not search in any directory higher than your home directory, even if it is on the list to scan.
+
+Once you have finished tinkering with the settings, there are three buttons at the bottom. The \"Revert to Default Settings\" button will immediately change everything back to the way it was when you first installed the app. This is not reverseable, so you'll have to manually make any changes you want again.
+
+The \"Cancel Changes\" button will revert any changes you made to the settings since the last time you clicked either of the other two buttons.
+
+The \"Apply Changes\" button will save the changes and apply them to all future scans."""
+
+        instructions.insert('1.0', scan_title)
+        instructions.insert('4.0', scan_instructions)
+        instructions.insert('15.0', settings_title)
+        instructions.insert('18.0', settings_instructions)
+        
+        instructions.tag_add("scanning", '1.0', "1.20")
+        instructions.tag_config("scanning", font=BODY_TEXT, justify="center")
+        
+        instructions.tag_add("settings", '15.0', "15.22")
+        instructions.tag_config("settings", font=BODY_TEXT, justify="center")
+        
+        instructions.config(state="disabled")
+        
+        # scan_title.grid(column=0, row=0)
+        instructions.grid(column=0, row=1, columnspan=3, sticky="nsew", padx=10)
+        
+        
+            
 
 import sys
 print("Python:", sys.version)
